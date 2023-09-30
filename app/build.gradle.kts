afbdbc1f8b88608c271c4e15492e6a2f7d09b374
cristiano <cekeaudio@gmail.com>
2023-09-30 11:08:32 -0300

    Meu-Primeiro-App-Dio

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+}
+
+android {
+    namespace = "com.example.meuprimeiroappdio"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.meuprimeiroappdio"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+
+}
---
+++ app/src/main/java/com/example/meuprimeiroappdio/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.meuprimeiroappdio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.igorbag.meuprimeiroappdio.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
